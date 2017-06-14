969068f8439b14c27a4b14ffa6544af5f9252c87
Sushma Badam <sushma.badam@ggktech.local>
2017-06-14 12:23:49 +0530

    Program.cs

--- Desktop/sushma/Program.cs
+++ Desktop/sushma/Program.cs
@@ -1,69 +1,46 @@
-﻿using System;
+using System;
 
-namespace sushma
+namespace add
 {
     class Program
     {
         static void Main(string[] args)
-        {
-           try{
-            Console.WriteLine("enter first number");
-            String num1 = Console.ReadLine();   
-            int n = num1.Length;
-            int[] p = new int[n]; 
-            int j=0;
-            foreach ( Char i in num1.ToCharArray() ) {
-                p[j] = Int32.Parse(i.ToString());
-                j++;
-                }
-                Console.Write("first number is : ");
-                for ( int l=0; l < n ; l++)
-                Console.Write(p[l]);
-                Console.WriteLine();
-            Console.WriteLine("enter second number");           
-            String num2 = Console.ReadLine();
-            int m = num2.Length;
-             int[] q = new int[m]; 
-             int k=0;
-            foreach (char r in num2.ToCharArray() ) {
-                q[k] = Int32.Parse(r.ToString());
-                k++;
-                }
-                 Console.Write("second number is : ");
-                for (int s=0; s < m; s++ )
-                Console.Write(q[s]);
-                Console.WriteLine();
-                Array.Reverse(p);
-                Array.Reverse(q);
-                int x;
-               if ( n < m )
-               {
-                    x = m;
-               }
-               else
-               {
-                    x = n;
-               }
-                    
-                int[] result = new int[x+1];
-                int carry = 0;
-                for ( int i=0; i <x ; i++){
-                    int y = carry+p[i] + q[i];
-                    carry = y/10; 
-                    y = y%10;
-                    result[i] = y;
-                }
-                result[x] = carry;
-                 Array.Reverse(result);
-                for ( int i=0; i <= x; i++) {
-                    Console.Write(result[i]);
-                }
-                }
-           catch (Exception e){
-               Console.WriteLine(e);
-
-           }
-
+        {          
+            int padlength = Math.Abs(args[0].Length - args[1].Length);
+            string sp = new string('0', padlength);
+            args[0] = (args[0].Length < args[1].Length) ? sp+args[0] : args[0];
+            args[1] = (args[1].Length < args[0].Length) ? sp+args[1] : args[1];
+            int max = (args[0].Length < args[1].Length) ? args[1].Length : args[0].Length;
+            int[] p = new int[max];
+            int[] q = new int[max];
+            int i = 0;
+            foreach (char ch in args[0].ToCharArray())
+            {
+                p[i] = Int32.Parse(ch.ToString());
+                i++;
+            }
+            i = 0;
+            foreach (char ch in args[1].ToCharArray())
+            {
+                q[i] = Int32.Parse(ch.ToString());
+                i++;
+            }
+            Array.Reverse(p);
+            Array.Reverse(q);
+            int[] result = new int[max+1];
+            int carry = 0;
+            for ( i=0; i<max; i++)
+            {
+                int t = p[i] + q[i] + carry; 
+                carry = t / 10;
+                 result[i] = t % 10;
+            }
+            result[max] = carry;
+            Array.Reverse(result);
+            for(int j=0; j <= max; j++)
+            {
+                Console.Write(result[j]);
+            }
         }
     }
 }
